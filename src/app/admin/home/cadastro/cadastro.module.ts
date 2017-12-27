c9f1814d79a7e01dee9b5f68700e3749f9882ab7
Douglas <dodo_1828@hotmail.co>
2017-12-27 14:05:52 -0200

    rita imoveis

--- src/app/admin/home/cadastro/cadastro.module.ts
+++ src/app/admin/home/cadastro/cadastro.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CadastroComponent } from './cadastro.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MaterializeModule} from 'angular2-materialize';
+import {AccordionModule, FileUploadModule, MessageModule, MessagesModule} from 'primeng/primeng';
+
+import {CadastroComponent} from './cadastro.component';
 import {MaterialDesignComponentsModule} from '../../../material-design-components/material-design-components.module';
-import {AccordionModule, FileUploadModule} from 'primeng/primeng';
-import { FormResidencialComponent } from './form-residencial/form-residencial.component';
-import { FormComercialComponent } from './form-comercial/form-comercial.component';
+import {FormResidencialComponent} from './form-residencial/form-residencial.component';
+import {FormComercialComponent} from './form-comercial/form-comercial.component';
 import {CadastroService} from './cadastro.service';
 
 @NgModule({
@@ -17,15 +18,18 @@ import {CadastroService} from './cadastro.service';
     MaterializeModule,
     MaterialDesignComponentsModule,
     AccordionModule,
-    FileUploadModule
+    FileUploadModule,
+    MessagesModule,
+    MessageModule
   ],
   declarations: [
     CadastroComponent,
     FormResidencialComponent,
     FormComercialComponent
   ],
-  providers:[
+  providers: [
     CadastroService
   ]
 })
-export class CadastroModule { }
+export class CadastroModule {
+}
